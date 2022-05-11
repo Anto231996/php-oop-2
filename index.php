b9d0075ec910df4db87e4b652e4b31c447571f1f
Anto231996 <tony2396@hotmail.it>
2022-05-11 11:41:19 +0200

    commit

--- index.php
+++ index.php
@@ -4,6 +4,11 @@ include_once __DIR__ .  './classes/CreditCard.php';
 include_once __DIR__ .  './classes/Food.php';
 include_once __DIR__ .  './classes/Product.php';
 
+$croccantini = new Food ('Manzo e verdure','Monge',6.0,'Croccantini',9.99,'Favolosi croccantini per cani');
+$cuccia = new Product ('Cuccia', 39.99, 'Stupenda cuccia per cani');
+$creditCard = new CreditCard ('5000 4000 3000 2000', 2022);
+$user = new User ('Salvo', 'Errori', $creditCard, true);
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -14,8 +19,31 @@ include_once __DIR__ .  './classes/Product.php';
     <title></title>
 </head>
 <body>
-    <?php
-    
-    ?>
+    <h3>Nome:</h3>
+    <?php echo $user->getFirstName(); ?>
+    <h3>Cognome:</h3>
+    <?php echo $user->getLastName(); ?>
+    <h3>Numero carta di credito:</h3>
+    <?php echo $creditCard->getNumber(); ?>
+    <h3>Scadenza carta di credito:</h3>
+    <?php echo $creditCard->getExpirty(); ?>
+    <h3>Prodotto:</h3>
+    <?php echo $cuccia->getName(); ?>
+    <h3>Prezzo:</h3>
+    <?php echo $cuccia->getPrice().' '.'€'; ?>
+    <h3>Descrizione:</h3>
+    <?php echo $cuccia->getDescription(); ?>
+    <h3>Prodotto:</h3>
+    <?php echo $croccantini->getName(); ?>
+    <h3>Brand:</h3>
+    <?php echo $croccantini->getBrand(); ?>
+    <h3>Peso:</h3>
+    <?php echo $croccantini->getWeight().' '.'kg'; ?>
+    <h3>Al gusto:</h3>
+    <?php echo $croccantini->getTaste(); ?>
+    <h3>Descrizione:</h3>
+    <?php echo $croccantini->getDescription(); ?>
+    <h3>Prezzo:</h3>
+    <?php echo $croccantini->getPrice().' '.'€'; ?>
 </body>
 </html>
